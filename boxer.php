ba0f0c8b569b5ad61116d80b1225c5ac27d58d6e
Kirill <akvamarin0610@gmail.com>
2023-11-26 19:05:45 +0400

    boxer coach

--- boxer.php
+++ boxer.php
@@ -2,11 +2,42 @@
     include('temp/server.php'); //подключение базы данных к странице
     include('temp/header.php'); //подключение шапки к странице
 ?>
+<div class="user_conditions">
+<h1><b>Наша гордость</b></h1>
+<div class="boxers">
 
+<?php
+    $push = $BOX_DB->query('SELECT * FROM `boxer`JOIN users on users.id_user = boxer.id_user');  //запрос через PDO, который узнаёт информацию о боксерах
+    $push->setFetchMode(PDO::FETCH_ASSOC);  //отправление запроса
+    while($row = $push->fetch()){ // возрат данных в ввиде асоциативного массива
+?>
 
+    <div class="boxer">
+        <div class="img_boxer">
+            <img src="img/boxer/<?php echo $row['img']; ?>" alt="boxer">
+        </div>
+        <div class="information_boxer">
+            <h2><b>
 
+            <?php 
+            echo $row['surname']; // вывод ФИО коментатора
+            echo " ";
+            echo $row['name'];
+            echo " ";
+            echo $row['patronymic']; 
+            ?>
 
+            </b></h2>
+            <p><?php echo $row['text_boxer']; //вывод информации о боксере ?></p>
+        </div>
+    </div>
 
+<?php
+    }
+?>
+
+</div>
+</div>
 
 
 
--- coach.php
+++ coach.php
@@ -2,9 +2,42 @@
     include('temp/server.php'); //подключение базы данных к странице
     include('temp/header.php'); //подключение шапки к странице
 ?>
+<div class="user_conditions">
+<h1><b>Наши тренера</b></h1>
+<div class="boxers">
 
+<?php
+    $push = $BOX_DB->query('SELECT * FROM `coach`JOIN users on users.id_user = coach.id_user');  //запрос через PDO, который узнаёт информацию о боксерах
+    $push->setFetchMode(PDO::FETCH_ASSOC);  //отправление запроса
+    while($row = $push->fetch()){ // возрат данных в ввиде асоциативного массива
+?>
 
+    <div class="boxer">
+        <div class="img_boxer">
+            <img src="img/coach/<?php echo $row['img']; ?>" alt="coach">
+        </div>
+        <div class="information_boxer">
+            <h2><b>
 
+            <?php 
+            echo $row['surname']; // вывод ФИО коментатора
+            echo " ";
+            echo $row['name'];
+            echo " ";
+            echo $row['patronymic']; 
+            ?>
+
+            </b></h2>
+            <p><?php echo $row['text_coach']; //вывод информации о тренере ?></p>
+        </div>
+    </div>
+
+<?php
+    }
+?>
+
+</div>
+</div>
 
 
 
